36b831ba438e95c56993791020fa14327608a060
Oliwier <276085@student.pwr.edu.pl>
2025-01-29 23:38:27 +0100

    update settings.py

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,12 +89,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Database1', # change to your database name
-        'USER': 'Admin', # change to your user name
-        'PASSWORD': 'ArdaksEN2004', # change to your user password
-        'HOST': 'localhost',
-        'PORT': '5432', # change only if you changed the default port during PostgreSQL installation
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('DATABASE_NAME'),
+    'USER': os.getenv('DATABASE_USER'),
+    'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+    'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+    'PORT': os.getenv('DATABASE_PORT', 5432),
     }
 }
 
